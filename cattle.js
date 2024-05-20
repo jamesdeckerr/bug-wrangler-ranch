f578573746aeeab22df8030d769c2a74d21ef478
James Decker <jdecker894@gmail.com>
2024-05-20 11:49:28 -0500

    Self Assessment Code Completed

--- cattle.js
+++ cattle.js
@@ -4,13 +4,25 @@ const roundup = (herdSize) => {
     const cattle = []
     const types = database.cattleTypes
 
-    for (let counter = 0; counter < size; counter++) {
+    for (let counter = 0; counter < herdSize; counter++) {
         const randomType = Math.floor(Math.random() * types.length)
         const animal = types[randomType]
         cattle.push(animal)
     }
 
-    return cattle
+let breeds = '';
+let i = 0;
+
+while (i < cattle.length) {
+    breeds += cattle[i].breed;
+    if (i < cattle.length - 1) {
+        breeds += ', ';
+    }
+    i++;
 }
+
+    return { breeds, cattle}
+}
+
 
 module.exports = { roundup }
--- drovers.js
+++ drovers.js
@@ -1,15 +1,16 @@
 const { database } = require("./database.js")
 
-const hireDrovers = (herdSize) => {
-    const drovers = []
+const cattleToDrive = 50
+const hireDrovers = (cattleToDrive) => {
+    const droversOut = []
     const allDrovers = database.drovers
     const numberNeeded = cattleToDrive / 10
 
     for (let counter = 0; counter < numberNeeded; counter++) {
         const randomHerderId = Math.floor(Math.random() * allDrovers.length)
-        drovers.push(allDrovers[randomHerderId])
+        droversOut.push(allDrovers[randomHerderId])
     }
-
+return droversOut
 }
 
 module.exports = { hireDrovers }
